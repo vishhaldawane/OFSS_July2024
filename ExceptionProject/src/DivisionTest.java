a6d46f466c0917a25ae42c118db034e20d25cb3e
vishhaldawane <vishalviniing@gmail.com>
2024-07-27 11:56:23 +0530

    Exceptions with Division

--- ExceptionProject/src/DivisionTest.java
+++ ExceptionProject/src/DivisionTest.java
@@ -1,16 +1,51 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+//The JPL - James Gosling Ken Arnold David Holmes
 
 public class DivisionTest {
 	public static void main(String[] args) throws IOException
 	{
 	
-		System.out.println("1. Begin main\n");
-		//Unhandled exception type FileNotFoundException
+		System.out.println("Begin main\n");
+			
+		Calculator calci = new Calculator();
+		calci.divide(100, 20);
+		calci.divide(100, 10);
+		calci.divide(100, 0);
+		calci.divide(100, 16);
+		calci.divide(100, 35);
+		
+		
+		
+		System.out.println("\nEnd of main");
+		
+	}
+}
+
+class Calculator
+{
+	
+	void divide(int num, int deno) 
+	{
+		System.out.println("Dividing "+num+"/"+deno);
+		try {
+			int div = num / deno;
+			System.out.println("Division : "+div);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Cannot divide by zero...");
+		}
+	}
+	
+}
+
+
+
+//Unhandled exception type FileNotFoundException
 		//Unhandled exception type IOException
 		
-		
+		/*
 			System.out.println("Trying to open the file....");
 			FileInputStream fin = new FileInputStream("/Users/apple/a.txt");
 			System.out.println("File is ready....");
@@ -21,28 +56,7 @@ public class DivisionTest {
 			}
 			fin.close();
 			System.out.println("File is closed....");
+	*/
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*
-		
-		
-		int z = 100 / 0;
-		System.out.println("8. Division : "+z);*/
-		
-		System.out.println("\n9.End of main");
-		
-	}
-}
 
-class Calculator
-{
-	
-}
+

3153d4fef5042d9b651cd0be1567778e7e10ca6e
vishhaldawane <vishalviniing@gmail.com>
2024-07-29 17:24:16 +0530

    TreeSet

--- CollectionProject/src/ArrayListTest.java
+++ CollectionProject/src/ArrayListTest.java
@@ -1,16 +1,69 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.TreeSet;
 
 public class ArrayListTest {
 	public static void main(String[] args) {
-		PhoneLog log1 = new PhoneLog("Dialled","Reeta",LocalDateTime.of(2024, 7, 27, 10, 30));
-		PhoneLog log2 = new PhoneLog("Missed","Meeta",LocalDateTime.of(2024, 7, 27, 20, 30));
-		PhoneLog log3 = new PhoneLog("Recieved","Neeta",LocalDateTime.of(2024, 7, 28, 11, 45));
-		PhoneLog log4 = new PhoneLog("Dialled","Seeta",LocalDateTime.of(2024, 7, 29, 12, 34));
-		PhoneLog log5 = new PhoneLog("Missed","Jeeta",LocalDateTime.of(2024, 7, 29, 9, 54));
+		PhoneLog log1 = new PhoneLog(3,"Dialled","Reeta",LocalDateTime.of(2024, 7, 27, 10, 30));
+		PhoneLog log2 = new PhoneLog(4,"Missed","Meeta",LocalDateTime.of(2024, 7, 27, 20, 30));
+		PhoneLog log3 = new PhoneLog(5,"Recieved","Neeta",LocalDateTime.of(2024, 7, 28, 11, 45));
+		PhoneLog log4 = new PhoneLog(2,"Dialled","Seeta",LocalDateTime.of(2024, 7, 29, 12, 34));
+		PhoneLog log5 = new PhoneLog(1,"Missed","Jeeta",LocalDateTime.of(2024, 7, 29, 9, 54));
 
-		System.out.println("Creating array as Container...");
+		
+		//10
+		System.out.println("Container as growable array....");
+		//ArrayList<PhoneLog> listLogs = new ArrayList<PhoneLog>();
+		//LinkedList<PhoneLog> listLogs = new LinkedList<PhoneLog>();
+		TreeSet<PhoneLog> listLogs = new TreeSet<PhoneLog>();
+		//HashSet<PhoneLog> listLogs = new HashSet<PhoneLog>();
+		
+		System.out.println("Adding log to the Container....");
+		listLogs.add(log1); //1
+		listLogs.add(log2); //2 
+		listLogs.add(log3); //3
+		listLogs.add(log4); //4
+		listLogs.add(log5); //5
+		Iterator<PhoneLog> iterator = listLogs.iterator();
+		while(iterator.hasNext()) {
+			PhoneLog theLog = iterator.next();
+			System.out.println("The Log : "+theLog);
+		}
+		
+	}
+}
+class PhoneLog implements Comparable<PhoneLog>
+{
+	int num;
+	String logType; // missed, recieved, dialled
+	String contact;
+	LocalDateTime timestamp;
+	
+	public PhoneLog(int num,String logType, String contact, LocalDateTime timestamp) {
+		super();
+		this.num=num;
+		this.logType = logType;
+		this.contact = contact;
+		this.timestamp = timestamp;
+	}
+	@Override
+	public String toString() {
+		return "PhoneLog [num="+num+" logType=" + logType + ", contact=" + contact + ", timestamp=" + timestamp + "]";
+	}
+	@Override
+	public int compareTo(PhoneLog o) {
+		System.out.println("Comparing "+num+" with "+o.num +" value : "+(num-o.num));
+		//return logType.compareTo(o.logType);
+		return Integer.compare(num, o.num);
+	}
+	
+	
+}
+		/*
+		 System.out.println("Creating array as Container...");
 
 		PhoneLog allLogs[] = new PhoneLog[5];
 		   
@@ -28,49 +81,12 @@ public class ArrayListTest {
 		}
 		
 		System.out.println("======================");
+		 
+		 */
 		
-		//10
-		System.out.println("ArrayList as growable array....");
-		ArrayList<PhoneLog> listLogs = new ArrayList<PhoneLog>();
 		
-		System.out.println("Adding log to the ArrayList....");
-		listLogs.add(log1);
-		listLogs.add(log2);
-		listLogs.add(log3);
 		
-		System.out.println("Iterating through the ArrayList....");
-		Iterator<PhoneLog> iterator = listLogs.iterator();
-		while(iterator.hasNext()) {
-			PhoneLog theLog = iterator.next();
-			System.out.println("The Log : "+theLog);
-		}
-		System.out.println("==============");
-		listLogs.add(log4);
-		listLogs.add(log5);
-		iterator = listLogs.iterator();
-		while(iterator.hasNext()) {
-			PhoneLog theLog = iterator.next();
-			System.out.println("The Log : "+theLog);
-		}
 		
-	}
-}
-class PhoneLog
-{
-	String logType; // missed, recieved, dialled
-	String contact;
-	LocalDateTime timestamp;
-	
-	public PhoneLog(String logType, String contact, LocalDateTime timestamp) {
-		super();
-		this.logType = logType;
-		this.contact = contact;
-		this.timestamp = timestamp;
-	}
-	@Override
-	public String toString() {
-		return "PhoneLog [logType=" + logType + ", contact=" + contact + ", timestamp=" + timestamp + "]";
-	}
-	
-	
-}
+		
+		
+		
